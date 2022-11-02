e1f86d3eede8dd5c099e5602e02f62f8f406a9d4
Leonardo <ljcl79@gmail.com>
2022-11-02 14:38:20 -0300

    Aula1

---
+++ SistemaAgencia/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string url = "sitiosweb?parametros";
+//            0123456789
+
+
+              
+Console.WriteLine(url);
+
+string parametros = url.Substring(9);
+
+Console.WriteLine(parametros);
+
+Console.ReadLine();
+
