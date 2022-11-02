d8fd1025ce2a9f8550b5c15bbbbdcd8e56104cb7
Leonardo <ljcl79@gmail.com>
2022-11-02 14:40:19 -0300

    Aula4

--- SistemaAgencia/Program.cs
+++ SistemaAgencia/Program.cs
@@ -2,13 +2,31 @@
 
 using SistemaAgencia;
 using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
 
 try
 {
-    //El usuario tiene el telefono 1234-42545
-    //Ahora este usuario cambió su telefono para 5436 22455
-    //Y para finalizar el usuario tiene otro telefono que es 23232323232
+    //El usuario tiene el telefono 1234-4254
+    // Mi telefono es 4231-9877 y puedo conversar en horas comerciales.
+    // Para mayor atención llamar al 5599-7485
 
+    //string patron = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+    //string patron = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+    //string patron = "[0-9]{4}[-][0-9]{4}";
+    //string patron = "[0-9]{4,5}[-][0-9]{4}";
+    //string patron = "[0-9]{4,5}-{0,1}[0-9]{4}";
+    string patron = "[0-9]{4,5}-?[0-9]{4}";
+
+    string cadena = "Mi telefono es 942319877 y puedo conversar en horas comerciales.";
+
+    Regex regex = new Regex(patron);
+
+    Console.WriteLine("Existe el patron?" + regex.IsMatch(cadena));
+
+    Console.WriteLine("El patron es:" + regex.Match(cadena));
+
+
+    /*
     string url = "https://www.bytebank.com?monedaOrigen=Pesos&monedaDestino=Dolares&cantidad=1500";
 
     Console.WriteLine(url.StartsWith("https://www.bytebank.com"));
